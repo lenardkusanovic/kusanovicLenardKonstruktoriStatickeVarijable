c26445051ad44792dbe92ab7bec321decadb4d78
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2020-12-01 21:59:12 +0100

    Add files via upload

---
+++ kusanovicLenardKonstruktoriStatickeVarijable(B)/kusanovicLenardKonstruktoriStatickeVarijable(B)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardKonstruktoriStatickeVarijable_B_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa("cvek", "kcve");
+            Console.WriteLine(x.ToString());
+
+            PrvaKlasa y = new PrvaKlasa("tomc", "matija");
+            Console.WriteLine(y.ToString());
+
+            PrvaKlasa z = new PrvaKlasa("kostro", "genije");
+            Console.WriteLine(z.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ kusanovicLenardKonstruktoriStatickeVarijable(B)/kusanovicLenardKonstruktoriStatickeVarijable(B)/PrvaKlasa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardKonstruktoriStatickeVarijable_B_
+{
+    class PrvaKlasa
+    {
+        private static int counter;
+        string ime;
+        string prezime;
+
+        public PrvaKlasa()
+        {
+            Counter++;
+        }
+
+        public PrvaKlasa(string ime , string prezime)
+        {
+            Counter++;
+            this.Ime = ime;
+            this.Prezime = prezime;
+
+        }
+
+        public static int Counter { get => counter; set => counter = value; }
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+
+        public override string ToString()
+        {
+            string ispis = "Redni broj: " + counter + ", " + ime " " + prezime;
+            return ispis;
+        }
+    }
+}
